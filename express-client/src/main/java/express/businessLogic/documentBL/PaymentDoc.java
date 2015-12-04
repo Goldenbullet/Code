e36509afe3ab328b1ad40211d099c3936024fbe8
Pangyk <1403842152@qq.com>
2015-12-05 03:12:36 +0800

    express v3.0.3

--- express-client/src/main/java/express/businessLogic/documentBL/PaymentDoc.java
+++ express-client/src/main/java/express/businessLogic/documentBL/PaymentDoc.java
@@ -1,15 +1,8 @@
 package express.businessLogic.documentBL;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import express.businessLogic.syslogBL.SysLog;
 import express.businesslogicService.financialBLService.PaymentBLService;
-import express.businesslogicService.managerBLService.SysLogBLService;
-import express.data.logData.LogIO;
 import express.dataService.documentDataService.PaymentDocDataService;
-import express.po.LogPO;
 import express.rmi.RMIClient;
 import express.vo.DateAvailableVO;
 import express.vo.PaymentDocVO;
@@ -38,7 +31,7 @@ public class PaymentDoc implements PaymentBLService{
 	}
 
 	public void endPaymentDoc() {
-		SysLogBLService log=new SysLog();
+		SysLog log=new SysLog();
 		log.addSysLog("生成付款单");
 	}
 
